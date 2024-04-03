076824944e48c0a0484a57e322d3671be52fa594
Parth Dave <daveparth422@gmail.com>
2024-04-04 01:03:24 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,7 +3,7 @@
 // promisis code for db connection
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
